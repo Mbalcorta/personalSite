80a3d42239cfbc724c55dc607a0507be183fe8c6
mateo <misterrattail@gmail.com>
2020-07-20 19:46:40 -0700

    Add images and to header.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Column, Row } from 'simple-flexbox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <Row vertical='center'>
+            <Column flexGrow={1} horizontal='center'>
+              <span> work </span>
+            </Column>
+            <Column flexGrow={3} horizontal='center'>
+              <h1> Mateo Balcorta </h1>
+            </Column>
+            <Column flexGrow={1} horizontal='center'>
+              <span> about </span>
+            </Column>
+          </Row>
+          <Row vertical='center'>
+            <Column flexGrow={1} horizontal='center'>
+              <span className='about'> About I’m a software engineer. I'm fascinated by programming and the exponential benefits it produces. My curiosity of how technology works keeps me engaged and presistantly pushing my limits of what is possible.</span>
+            </Column>
+          </Row>
       </header>
-    </div>
+      <Row >
+      <Column flexGrow={1}>
+        <a><img src={require('./block.png')}/></a>
+      </Column>
+      <Column flexGrow={1}>
+      <a><img src={require('./progressive.png')}/></a>
+      </Column>
+      </Row>
+      <Row >
+      <Column flexGrow={1}>
+        <a><img src={require('./demand.png')}/></a>
+      </Column>
+      <Column flexGrow={1}>
+      <a><img src={require('./fcc.png')}/></a>
+      </Column>
+      </Row>
+    </div> 
   );
 }
 
